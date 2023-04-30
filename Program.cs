492a2d77f4957e6380c6c7cd90e44f5987d4f290
ofcdougls <ofc.dougls@outlook.com>
2023-04-30 17:56:12 -0300

    Atividade concluida

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("--- Múltiplo ---\n");
+
+Console.Write("Digite um número...........: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Avaliar se é múltiplo de...: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+if (n1 % n2 == 0)
+{
+    Console.WriteLine($"\n{n1} é múltiplo de {n2}");
+}
+else
+{
+    Console.WriteLine($"\n{n1} não é múltiplo de {n2}");
+}
